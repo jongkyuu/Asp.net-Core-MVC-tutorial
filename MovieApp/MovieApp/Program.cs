28237cb4562f066b9c5c300fd60dbfd28faaa289
minerva421 <minerva421.ai@gmail.com>
2023-02-27 14:53:14 +0900

    스캐폴딩 추가

--- MovieApp/MovieApp/Program.cs
+++ MovieApp/MovieApp/Program.cs
@@ -1,3 +1,6 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MovieApp.Data;
 namespace MovieApp
 {
     public class Program
@@ -5,6 +8,8 @@ namespace MovieApp
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<MovieAppContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("MovieAppContext") ?? throw new InvalidOperationException("Connection string 'MovieAppContext' not found.")));
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
