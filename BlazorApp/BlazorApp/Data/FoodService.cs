133df89e6db31bfc2b4ad5aefbb131f6eea44ae7
minerva421 <minerva421.ai@gmail.com>
2023-03-01 22:47:29 +0900

    Razor 프로젝트 추가

---
+++ BlazorApp/BlazorApp/Data/FoodService.cs
@@ -0,0 +1,21 @@
+﻿namespace BlazorApp.Data
+{
+    public class Food
+    {
+        public string Nmae { get; set; }
+        public string Price { get; set; }
+    }
+
+    public class FoodService
+    {
+        public List<Food> GetFoods()
+        {
+            return new List<Food>
+            {
+                new Food() { Nmae="A", Price="10000" },
+                new Food() { Nmae="B", Price="15000" },
+                new Food() { Nmae="C", Price="20000" },
+            };
+        }
+    }
+}
