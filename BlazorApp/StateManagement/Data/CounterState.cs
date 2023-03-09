6e3badd996f3a53e660be9ae9f80f90fb3fd502d
minerva421 <minerva421.ai@gmail.com>
2023-03-09 12:51:39 +0900

    State 관리

---
+++ BlazorApp/StateManagement/Data/CounterState.cs
@@ -0,0 +1,23 @@
+﻿namespace StateManagement.Data
+{
+    public class CounterState
+    {
+        private int _count = 0;
+        public Action? OnStateChanged;
+
+        public int Count
+        {
+            get => _count;
+            set
+            {
+                _count = value;
+                Refresh();
+            }
+        }
+
+        private void Refresh()
+        {
+            OnStateChanged?.Invoke();
+        }
+    }
+}
