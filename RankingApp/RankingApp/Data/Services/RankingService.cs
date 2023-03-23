41939d541829fe46244fb8d9164fe3e796a3fb29
minerva421 <minerva421.ai@gmail.com>
2023-03-23 13:16:08 +0900

    RankingApp 3 작성중

--- RankingApp/RankingApp/Data/Services/RankingService.cs
+++ RankingApp/RankingApp/Data/Services/RankingService.cs
@@ -12,6 +12,9 @@ namespace RankingApp.Data.Services
             _context = context;
         }
 
+        // CRUD : Create, Read, Update, Delete
+
+        // Raed
         public Task<List<GameResult>> GetGameResultAsync()
         {
             List<GameResult> results = _context.GameResults
@@ -19,6 +22,14 @@ namespace RankingApp.Data.Services
 
 
             return Task.FromResult(results);
+        }
+
+        public Task<GameResult> AddGameResultAsync(GameResult gameResult)
+        {
+            _context.GameResults.Add(gameResult);
+            _context.SaveChanges();
+
+            return Task.FromResult(gameResult);
         }
     }
 }
